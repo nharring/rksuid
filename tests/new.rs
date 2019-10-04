61bef4fa3ded26ebc8636535f7997e5785b3f2d4
Nicholas Harring <nharring@users.noreply.github.com>
2019-10-04 11:15:01 -0700

    Update new.rs
    Updated imports to improve namespace readability, added test coverage for variations of gen_payload to fix coverage numbers.

--- tests/new.rs
+++ tests/new.rs
@@ -1,40 +1,49 @@
-extern crate rksuid;
 
 #[cfg(test)]
 mod tests{
-use ::rksuid::rksuid;
+use rksuid::rksuid::{new, RngType, Ksuid, gen_epoch, gen_payload};
 use chrono::prelude::*;
 use rand::distributions::Standard;
 use rand::prelude::*;
 use std::{thread, time};
-
+use strum::IntoEnumIterator;
+use std::any::{Any, TypeId};
 
 // Creation tests
 #[test]
-fn new() {
-    let first = rksuid::new(None, None);
+fn test_new() {
+    let first = new(None, None);
     thread::sleep(time::Duration::from_millis(2000));
-    let second = rksuid::new(None, None);
+    let second = new(None, None);
     assert_ne!(first.timestamp, second.timestamp);
 }
 #[test]
 fn new_with_timestamp() {
-    let ksuid = rksuid::new(Some(85), None);
+    let ksuid = new(Some(85), None);
     assert_eq!(ksuid.timestamp, 85);
 }
 #[test]
 fn new_with_payload() {
     let payload: u128 = StdRng::from_entropy().sample(Standard);
-    let ksuid = rksuid::new(None, Some(payload));
+    let ksuid = new(None, Some(payload));
     assert_eq!(payload, ksuid.payload);
 }
 #[test]
 fn new_with_payload_and_timestamp() {
     let payload: u128 = StdRng::from_entropy().sample(Standard);
-    let epoch_base = rksuid::gen_epoch();
+    let epoch_base = gen_epoch();
     let timestamp = Utc::now().signed_duration_since(epoch_base).num_seconds() as u32;
-    let ksuid = rksuid::new(Some(timestamp), Some(payload));
+    let ksuid = new(Some(timestamp), Some(payload));
     assert_eq!(ksuid.payload, payload);
     assert_eq!(ksuid.timestamp, timestamp);
+}
+
+
+#[test]
+fn test_payload_variants() {
+    for e in RngType::iter() {
+        let payload: u128 = gen_payload(Some(e));
+        assert_eq!(payload.type_id(), TypeId::of::<u128>());
+    }
 }
 }
